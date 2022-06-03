a3320daed7bb72ff894d27f9a317e0d163854c46
Dejan Svetec <dejan.svetec@icloud.com>
2022-06-03 12:07:13 +0200

    Implement action

---
+++ index.js
@@ -0,0 +1,36 @@
+import * as core from "@actions/core";
+import { NetlifyAPI } from "netlify";
+
+const setProductionBranch = async (netlifyClient, siteId, branch) => {
+  const site = await netlifyClient.updateSite({
+    site_id: siteId,
+    body: {
+      build_settings: {
+        branch: branch,
+      },
+    },
+  });
+
+  if (site.build_settings.repo_branch == branch) {
+    core.info(`The branch has been updated to '${branch}'.`);
+  } else {
+    core.setFailed("The branch hasn't been updated.");
+  }
+};
+
+const run = async () => {
+  try {
+    const netlifyToken = core.getInput("netlifyToken");
+    const siteId = core.getInput("siteId");
+    const branch = core.getInput("branch");
+    core.setSecret("netlifyToken");
+
+    const netlifyClient = new NetlifyAPI(netlifyToken);
+
+    await setProductionBranch(netlifyClient, siteId, branch);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+run();
